ebd0e3ca06452d130c334ebd10765e73a7a2f6dd
zcord <zeus.cordeiro@gmail.com>
2023-06-01 12:24:27 -0400

    Categories page begining to be filled in, main list and links set up

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,9 +1,32 @@
+import CategoryCard from "@/components/CategoryCard";
 
+const categories = async () => {
+  const res = await fetch("https://api.publicapis.org/entries");
+  const data = await res.json();
+  const categoriesList = data.entries.map((entry) => {
+    return entry.Category;
+  });
 
-const categories = () => {
+  const uniqueCategories = [...new Set(categoriesList)];
   return (
-    <div>categories</div>
-  )
-}
+    <>
+      <div>
+        <h1 className="text-center text-6xl font-bold italic underline text-blue-900 m-3">Categories</h1>
+        <div className="flex flex-wrap align-center justify-evenly">
+          {uniqueCategories.map((category) => {
+            const categoryName = category.replace(/\s+/g, "");
+            return (
+              <div className="m-2">
+                <a href={`/categories/${categoryName}`}>
+                <CategoryCard name={category} />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default categories
+export default categories;
---
+++ components/CategoryCard.jsx
@@ -0,0 +1,14 @@
+const CategoryCard = ({ name }) => {
+    return (
+      <div className="mt-10">
+        <div className="categoryCard hover:cursor-pointer text-center">
+          <header className="categoryCard-header">
+            <span className="title">{name}</span>
+          </header>
+          <div className="text-center text-black italic text-sm">See Available API's</div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default CategoryCard;
